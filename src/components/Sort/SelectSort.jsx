c0aeb7d1b08126684c4c662bb960578862ff19a9
GithubDat <GithubDat@users.noreply.github.com>
2021-11-12 13:14:30 +0530

    Update SelectSort.jsx

--- src/components/Sort/SelectSort.jsx
+++ src/components/Sort/SelectSort.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
 
+function SelectSort(props) {
+    return (
+      <select defaultValue="sort" onChange={(e) => props.handleSorting(e)}>
+        <option value="sort_by">Sort By</option>
+        <option value="likes">Likes</option>
+        <option value="createdDate">Created Date</option>
+      </select>
+    )
+}
+
+export default SelectSort
