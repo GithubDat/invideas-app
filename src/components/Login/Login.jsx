311b0a553c2b9015844972a3a264985967eb191c
GithubDat <GithubDat@users.noreply.github.com>
2021-11-12 13:10:38 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,1 +1,21 @@
+import React, { useState } from 'react';
+import './Login.css'
+import {Link} from 'react-router-dom';
 
+export default function Login({getId}) {
+    // const [id, getId] = useState();
+    
+    return (        
+        <div className='parent-container'>
+            <div className='cont'>
+                <div className='eid'>
+                    <input type='text' name='employeeId' placeholder='Enter employee ID' onChange={(e) => getId(e)}  required/>
+                </div>
+                
+            <div>
+               <Link to='/home'> <button className='Login_button'>Login</button></Link>
+             </div>   
+            </div>
+        </div>
+    )
+}
