9a8d1a6ade035917ac3d65b1485b5d4eb72a236d
GithubDat <GithubDat@users.noreply.github.com>
2021-11-12 13:12:21 +0530

    Create NewItem.jsx

---
+++ src/components/NewChallenge/NewItem.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./NewItem.css";
+
+function NewItem({ getData, submitData }) {
+  // const [select, setSelect] = useState("");
+  // const selectedField = (e, type) => {
+  //   console.log(e.target.value)
+  //   setSelect({ type: e.target.value })
+  // }
+
+  return (
+    <div className="rootDiv">
+      <div className="container parent">
+        <h2>Add your Idea or Challenge below</h2>
+        <div>
+          <label>Title :</label>
+          <input
+            type="text"
+            name="title"
+            id="challenge_2"
+            onChange={(e) => getData(e, "title")}
+          />
+        </div>
+        <div>
+          <label>Description :</label>
+          <input
+            type="text"
+            name="description"
+            id="challenge_3"
+            onChange={(e) => getData(e, "description")}
+          />
+        </div>
+
+        <div className="drop">
+          <label className="dropl">Tag :</label>
+          <select defaultValue="Select the tag" onChange={(e) => getData(e, "tag")}>
+            <option value="none">Select the type</option>
+            <option value="Tech">Tech</option>
+            <option value="Feature">Feature</option>
+          </select>
+        </div>
+        <button className="rdr_btn" onClick={() => submitData()}>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default NewItem;
